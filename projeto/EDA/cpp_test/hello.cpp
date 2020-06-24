22bc6791dbcf22ba76c621dc424d169106a9ec5e
Israel Oliveira <prof.israel@gmail.com>
2020-06-24 19:21:42 -0300

    PoC calling C++ with Python done!

--- projeto/EDA/cpp_test/hello.cpp
+++ projeto/EDA/cpp_test/hello.cpp
@@ -21,11 +21,7 @@ using Eigen::MatrixXd;
 
 double greet()
 {
-  MatrixXd m(2,2);
-  m(0,0) = 3;
-  m(1,0) = 2.5;
-  m(0,1) = -1;
-  m(1,1) = m(1,0) + m(0,1);
+  MatrixXd m = MatrixXd::Random(2,2);
   return m.norm();
 }
 
