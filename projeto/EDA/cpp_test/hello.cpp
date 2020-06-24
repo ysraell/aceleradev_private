2a015148148d99ff2c0e6522cd631d74dc93152d
Israel Oliveira <prof.israel@gmail.com>
2020-06-24 19:08:23 -0300

    PoC calling C++ with Python done!

---
+++ projeto/EDA/cpp_test/hello.cpp
@@ -0,0 +1,36 @@
+/* 
+Copyright 2018 Sahaj Software Solutions, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include <boost/python.hpp>
+#include <Eigen/Dense>
+
+using Eigen::MatrixXd;
+
+double greet()
+{
+  MatrixXd m(2,2);
+  m(0,0) = 3;
+  m(1,0) = 2.5;
+  m(0,1) = -1;
+  m(1,1) = m(1,0) + m(0,1);
+  return m.norm();
+}
+
+BOOST_PYTHON_MODULE(hello)
+{
+    using namespace boost::python;
+    def("greet", greet);
+}
